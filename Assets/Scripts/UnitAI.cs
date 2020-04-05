5e4a874d4de20662caac6479b2a5b917fa0928f7
ligamt15 <37112222+ligamt15@users.noreply.github.com>
2020-04-05 15:07:36 +0300

    TELKA
    COMMIT

---
+++ Assets/Scripts/UnitAI.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitAI : MonoBehaviour
+{
+
+    public float viewAngle;
+    [Range(0, 360)]
+    public float viewRadius;
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+    public List<Transform> visibleTargets = new List<Transform>();
+    public Transform target;
+    public Transform way;
+    Vector3 destination;
+    NavMeshAgent agent;
+
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+        StartCoroutine("FindTargetWithDelay", .2f);
+
+    }
+
+    void Update()
+    {
+        destination = way.position;
+        agent.destination = destination;
+
+        if (target != null && Vector3.Distance(destination, target.position) > 1.0f)
+        {
+            destination = target.position;
+            agent.destination = destination;
+        }
+    }
+
+    IEnumerator FindTargetWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+            if (visibleTargets.Count > 0)
+            {
+                float distance = -1;
+                for (int i = 0; i < visibleTargets.Count; i++)
+                {
+                    if (distance == -1 || distance > Vector3.Distance(transform.position, visibleTargets[i].position))
+                    {
+                        distance = Vector3.Distance(transform.position, visibleTargets[i].position);
+                        target = visibleTargets[i];
+                    }
+                }
+            }
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Debug.Log(targetsInViewRadius[i].tag + " " + this.tag);
+            if (targetsInViewRadius[i].tag == this.tag) { continue; }
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+}
+
+
+
---
+++ Assets/Scripts/UnitStats2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitStats2 : MonoBehaviour
+{
+
+
+    public float curHealth;
+    public float maxHealth;
+    public float lengthHealth;
+    public int protect;
+    public int damage;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (curHealth <= 0)
+            Dead();
+
+    }
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+
+    public void ReceivingDamage(int recDamage)
+    {
+        curHealth -= recDamage * ((100 - protect) / 100f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("UnitPlayer2"))
+            curHealth -= damage * ((100 - protect) / 100f);
+
+    }
+
+
+}
+
